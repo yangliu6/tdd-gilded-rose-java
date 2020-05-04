35c52849302056edc7435f151c3d66fedd9dd3d3
yangliu <1695938413@qq.com>
2020-05-04 13:35:11 +0800

    【fix】the quality of common commodity should not less than 0

--- src/main/java/cn/xpbootcamp/gilded_rose/commodity/CommonCommodity.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/commodity/CommonCommodity.java
@@ -6,15 +6,23 @@ public class CommonCommodity extends Commodity{
 
     @Override
     public int calculateQuality(int day,int sellIn,int quality){
+        //保质期随天数的增加而减少
+        this.setSellIn(sellIn-day);
         //没过保质期，质量每天减少1
-       if(day <= sellIn){
+        if(day <= sellIn){
            this.setQuality(quality-day);
-       }
+        }
        //过了保质期，质量呈双倍减少
         else{
             int difference = day - sellIn;
             this.setQuality(quality-sellIn-2*difference);
         }
-        return this.getQuality();
+
+        if(this.getQuality()>0){
+            return this.getQuality();
+        }
+        else{
+            return 0;
+        }
     }
 }
